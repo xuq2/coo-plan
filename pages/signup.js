1b9e23c74a1a7ece213668843e2675263a1806f3
eugeneyjy <yonge@oregonstate.edu>
2022-03-09 20:53:22 -0800

    Logic for user sign up

---
+++ pages/signup.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+import SignUpContainer from '../components/SignUpContainer';
+
+export default function SignUp() {
+  return (
+    <Layout>
+      <SignUpContainer/>
+    </Layout>
+  );
+}
